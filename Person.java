84c1d2c57901f53ebe453369eaf82535403dd033
gaorongyi <gaorongyi@vip.qq.coom>
2015-05-21 17:02:48 +0800

    Hello Person Robot
    insert

---
+++ Person.java
@@ -0,0 +1,23 @@
+class Person{
+	private String name ;
+	private Integer age ;
+	private String card ;
+	public void setName(String name){
+		this.name = name ;
+	}
+	public String getName(){
+		return this.name ;
+	}
+	public void setAge(Integer age){
+		this.age = age ;
+	}
+	public Integer getAge(){
+		return this.age ;
+	}
+	public void setCard(String card){
+		this.card = card ;
+	}
+	public String getCard(){
+		this.card = card ;
+	}
+}
---
+++ Robot.java
@@ -0,0 +1,11 @@
+class Robot{
+	private String name ;
+	private Double price ;
+	private Date birthday ;
+	public void setName(String name){
+		this.name = name ;
+	}
+	public String getName(){
+		return this.name ;
+	}
+}
