3bab36d14e58d6170a5db24d5c3c71d3c5833e06
gaorongyi <gaorongyi@vip.qq.coom>
2015-05-22 09:53:39 +0800

    update Hello.java master

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World !") ;
-		System.out.println("") ;
+		System.out.println("update on branch master") ;
 	}
 }
